64400838acb3c02d8371c4479e4c9e5d44c6c258
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-09-18 12:23:44 -0600

    About page

--- src/pages/about_page.ts
+++ src/pages/about_page.ts
@@ -4,4 +4,18 @@ export class AboutPage extends Page {
   constructor() {
     super("About");
   }
+
+  set_active(): void {
+    super.set_active();
+    $(this.target_div_element).html(`
+      <p>
+        This is a simple web calculator for cryptocurrency trading. With it you can calculate the average cost of your trades (the "DCA/Dollar Cost Average")
+         and see what profit margins you would have at various price points. Additionally, the website will store all of your coins / transactions locally on
+         your computer so that you can come back to it later and not have to re-enter all of the values.
+      </p>
+      <p>
+        This application is free and open source and licensed under the MIT license. No information is collected or stored anywhere other than
+        your browser/computer. Please feel free to audit the source code or contribute to the project on
+         <a href="https://github.com/fredclausen/crypto-calc-web">GitHub</a>.`);
+  }
 }
--- src/pages/page.ts
+++ src/pages/page.ts
@@ -1,15 +1,17 @@
 export class Page {
   title: string;
-  constructor(title: string) {
+  target_div_element: string;
+  constructor(title: string, target_div_element: string | null = null) {
     this.title = title;
+    this.target_div_element = target_div_element || "#main";
   }
 
   set_active() {
     this.set_window_title_bar();
-    $("main").html(this.title);
+    $(this.target_div_element).html(this.title);
   }
 
   set_window_title_bar(): void {
-    document.title = this.title;
+    document.title = "Crypto Web Calculator: " + this.title;
   }
 }
